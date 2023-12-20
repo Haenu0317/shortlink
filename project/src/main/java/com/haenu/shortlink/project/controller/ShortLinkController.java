14703b2450c4eb48cdf2aab8861856ed35153bac
Haenu0317 <2523053533@qq.com>
2023-12-20 13:49:49 +0800

    feature: 短链接init

---
+++ project/src/main/java/com/haenu/shortlink/project/controller/ShortLinkController.java
@@ -0,0 +1,16 @@
+package com.haenu.shortlink.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接控制层
+ *
+ * @author haenu
+ * @version 1.0
+ * @date 2023/12/20 13:38
+ */
+@RestController
+public class ShortLinkController {
+
+
+}
