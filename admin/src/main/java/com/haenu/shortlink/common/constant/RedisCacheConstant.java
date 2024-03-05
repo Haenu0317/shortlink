3f3ec3e0cfbcca062c794bfa6b1d74030e8add7e
Haenu0317 <2523053533@qq.com>
2024-03-05 12:56:46 +0800

    optimize: 优化单个用户创建分组数量不能大于20

--- admin/src/main/java/com/haenu/shortlink/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/haenu/shortlink/common/constant/RedisCacheConstant.java
@@ -8,6 +8,15 @@ package com.haenu.shortlink.common.constant;
  * @date 2023/12/18 11:01
  */
 public class RedisCacheConstant {
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
     public static final String TOKEN_PREFIX = "login_";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
+
