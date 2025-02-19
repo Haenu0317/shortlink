347c70ae433be424ac95e77e3642fd91d5391c34
Haenu0317 <2523053533@qq.com>
2025-02-19 16:52:54 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/com/haenu/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.haenu.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.haenu.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
