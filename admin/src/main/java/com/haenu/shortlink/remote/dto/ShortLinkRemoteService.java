6f4fc7f0c590dc6dc2f0bbcba814568da1afd162
Haenu0317 <2523053533@qq.com>
2025-02-19 16:47:27 +0800

    feature: 开发短链接监控之分页访问记录接口

--- admin/src/main/java/com/haenu/shortlink/remote/dto/ShortLinkRemoteService.java
+++ admin/src/main/java/com/haenu/shortlink/remote/dto/ShortLinkRemoteService.java
@@ -7,10 +7,7 @@ import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.haenu.shortlink.common.convention.result.Result;
 import com.haenu.shortlink.remote.dto.req.*;
-import com.haenu.shortlink.remote.dto.resp.ShortLinkCreateRespDTO;
-import com.haenu.shortlink.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
-import com.haenu.shortlink.remote.dto.resp.ShortLinkPageRespDTO;
-import com.haenu.shortlink.remote.dto.resp.ShortLinkStatsRespDTO;
+import com.haenu.shortlink.remote.dto.resp.*;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
@@ -140,6 +137,22 @@ public interface ShortLinkRemoteService {
      */
     default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
         String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+
+    /**
+     * 访问单个短链接指定时间内监控访问记录数据
+     *
+     * @param requestParam 访问短链接监控访问记录请求参数
+     * @return 短链接监控访问记录信息
+     */
+    default Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        Map<String, Object> stringObjectMap = BeanUtil.beanToMap(requestParam, false, true);
+        stringObjectMap.remove("orders");
+        stringObjectMap.remove("records");
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats/access-record", stringObjectMap);
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
